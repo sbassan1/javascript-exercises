3669be99c209b32e532c54a5b6b3d510e2ecb56e
Santiago Bassani <santiagobassani2000@gmail.com>
2024-11-01 22:17:33 -0300

    javascript exercises from 1 to 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    if (times < 0) return "ERROR";
+    let res = ""
+    for(let i = 0; i < times; i++){
+        res += word;
+    }
+    return res;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(numA,numB) {
+    let sum = 0;
+
+    if (!Number.isInteger(numA) || !Number.isInteger(numB)) return "ERROR";
+    if (numA < 0 || numB < 0) return "ERROR"
+
+    let min;
+    let max;
+
+    if(numA < numB) { 
+        min = numA;
+        max = numB;
+    } else { 
+        min = numB; 
+        max = numA; 
+    }
+
+    for(let i = min ; i <= max ; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
